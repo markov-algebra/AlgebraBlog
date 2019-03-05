e7b981a0dad22c361550bec9f27b4bfd03c93052
markov-algebra <marko.vojtek@gmail.com>
2019-03-05 17:40:04 +0100

    show and edit update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -57,6 +57,8 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+       
+        return view('users.show', compact('user'));
     }
 
     /**
@@ -67,8 +69,24 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        //
+      $user_1 = $this->show($id);
+
+      //dd($user_1);
+
+
+      $user->name = $user_1['name'];
+      $user->email = $user_1['email'];
+      $user->password = $user_1['password'];
+     // $user->remember_token = 'j,hsbdljhcbdsyclysdhbclysdcbjxnč';
+      $user->save();
+
+      return redirect()->route('users.index')->withFlashMessage('Uspješno ste editirali postojećeg korisnika.');
     }
+       
+      //  $user->save();
+
+      //  return redirect()->route('users.index')->withFlashMessage('Uspješno ste dodali starog korisnika.');
+    //}
 
     /**
      * Update the specified resource in storage.
